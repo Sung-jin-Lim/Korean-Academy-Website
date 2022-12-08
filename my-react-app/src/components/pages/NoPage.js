c83186cc4d677f93a58e726b8548f50960ff0305
Sungjin Lim <85204388+Sung-jin-Lim@users.noreply.github.com>
2022-12-08 12:59:26 +0900

    Update NoPage.js

---
+++ my-react-app/src/components/pages/NoPage.js
@@ -0,0 +1,9 @@
+function NoPage() {
+  return (
+    <div className="noPage">
+      <h1>Nothing Here</h1>
+      <h6>The page you are looking for doesn't seem to exist</h6>
+    </div>
+  );
+}
+export default NoPage;
