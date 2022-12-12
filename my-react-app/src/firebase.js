dcd0615c255255952d1ecbd2ad7e440f054759ac
Sungjin Lim <85204388+Sung-jin-Lim@users.noreply.github.com>
2022-12-12 10:31:51 +0900

    Update firebase.js

--- my-react-app/src/firebase.js
+++ my-react-app/src/firebase.js
@@ -8,10 +8,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const one = process.env.REACT_APP_PROJECT_ID
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
+  projectId: one,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
