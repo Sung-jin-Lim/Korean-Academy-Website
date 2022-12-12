dbc2e44a46e42e6edab222699ca53eac863a8396
Sungjin Lim <85204388+Sung-jin-Lim@users.noreply.github.com>
2022-12-12 09:55:48 +0900

    fix  local.env file

--- my-react-app/src/firebase.js
+++ my-react-app/src/firebase.js
@@ -8,28 +8,28 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-//   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-//   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-//   appId: process.env.NEXT_PUBLIC_APP_ID,
-//   measurementId: process.env.NEXT_PUBLIC_API_KEY
-
-
-
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAs-GJWZBXmrY49aqPfvRQZXtM2l5F9JQE",
-  authDomain: "korean-academy.firebaseapp.com",
-  projectId: "korean-academy",
-  storageBucket: "korean-academy.appspot.com",
-  messagingSenderId: "397510767446",
-  appId: "1:397510767446:web:fdc865d9a6958d49761cf7",
-  measurementId: "G-5DS3LKSE2D"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_API_KEY
+
+
+
 };
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAs-GJWZBXmrY49aqPfvRQZXtM2l5F9JQE",
+//   authDomain: "korean-academy.firebaseapp.com",
+//   projectId: "korean-academy",
+//   storageBucket: "korean-academy.appspot.com",
+//   messagingSenderId: "397510767446",
+//   appId: "1:397510767446:web:fdc865d9a6958d49761cf7",
+//   measurementId: "G-5DS3LKSE2D"
+// };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
